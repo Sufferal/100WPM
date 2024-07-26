cdbab089d2f74b23a5248a60d943819fc56d42e6
Ciprian <48862508+Sufferal@users.noreply.github.com>
2024-07-26 13:21:12 +0300

    feat: add caret (#3)

--- src/components/WordItem.vue
+++ src/components/WordItem.vue
@@ -1,16 +1,36 @@
 <script setup>
+import { computed } from "vue";
+
 const props = defineProps({
   name: String,
   typedLetters: Array,
+  currentWordIndex: Number,
+  wordIndex: Number,
 });
 
+const currentLetterIndex = computed(() => props.typedLetters.length - 1);
+
 function getClass(index) {
-  if (index < props.typedLetters.length) {
-    return props.name[index] === props.typedLetters[index]
-      ? "text-correct"
-      : "text-error";
+  let className = "";
+
+  if (
+    index === 0 &&
+    props.typedLetters.length === 0 &&
+    props.currentWordIndex === props.wordIndex
+  ) {
+    className += "text-caret-first";
   }
-  return "";
+
+  if (index < props.typedLetters.length) {
+    const isCurrentLetter = index === currentLetterIndex.value;
+    const isCurrentWord = props.currentWordIndex === props.wordIndex;
+
+    return props.name[index] === props.typedLetters[index]
+      ? `text-correct ${isCurrentWord && isCurrentLetter ? "text-caret" : ""}`
+      : `text-error ${isCurrentWord && isCurrentLetter ? "text-caret" : ""}`;
+  }
+
+  return className;
 }
 </script>
 
@@ -29,5 +49,6 @@ function getClass(index) {
 <style scoped>
 #currentWord {
   font-size: 3rem;
+  letter-spacing: 0.1rem;
 }
 </style>
